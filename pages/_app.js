db27eecdb1f44f88a49764f99e5319088d0057fe
Ahzam Khan <ahzam.main@gmail.com>
2023-02-26 23:01:40 +0530

    nice description

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import "@/styles/globals.css";
 import "@fontsource/space-grotesk";
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>tinyhack</title>
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
