b320e571cc5e0af9d36460785e65ce18f9041ec6
Ahzam Khan <ahzam.main@gmail.com>
2023-03-22 17:25:37 +0530

    pivot landing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import { Flex } from "@chakra-ui/react";
-import Typewriter from "typewriter-effect";
-import Nav from "@/components/nav";
+import { Flex, Text, Button, Image } from "@chakra-ui/react";
 
 const art = `
 ████████╗██╗███╗   ██╗██╗   ██╗    ██╗  ██╗ █████╗  ██████╗██╗  ██╗
@@ -16,37 +14,76 @@ console.log(art);
 export default function index() {
   return (
     <Flex
-      minH={"100vh"}
-      background={"#FAFBF6"}
-      _dark={{ background: "#657039", color: "white" }}
+      background={"#181818"}
       flexDir={"column"}
-      align={"center"}
-      color={"black"}
+      color={"white"}
       cursor={"default"}
     >
-      <Nav />
-
-      <Flex align={"center"} h={"90vh"}>
+      <Flex h={"100vh"} gap={"76px"} flexDir={"column"} overflowY={"hidden"}>
         <Flex
-          fontSize={{ base: "28px", md: "32px", lg: "45px", xl: "55px" }}
-          fontWeight={"semibold"}
-          w={{ base: "250px", md: "400px", lg: "779px", xl: "779px" }}
+          marginTop={"113px"}
+          flexDir={"column"}
+          align={"center"}
+          gap={"28.72px"}
         >
-          <Typewriter
-            onInit={(typewriter) => {
-              typewriter
-                .typeString("securing smart contracts made easy.")
-                .pauseFor(2500)
-                .deleteAll()
-                .typeString("no smart contract is smart if it ain't secure.")
-                .pauseFor(2500)
-                .deleteAll()
-                .typeString(
-                  "your friendly neighbourhood smart contract bodyguard."
-                )
-                .start();
-            }}
-          />
+          <Flex gap={"16px"} flexDir={"column"} align={"center"}>
+            <Text fontSize={"34px"} fontWeight={"bold"}>
+              Tinyhack
+            </Text>
+            <Text
+              className="sub"
+              fontSize={"26px"}
+              fontWeight={"normal"}
+              w={"584px"}
+              textAlign={"center"}
+              opacity={"85%"}
+            >
+              a project built under{" "}
+              <a
+                href={"https://eif3.devfolio.co/"}
+                target={"_blank"}
+                rel={"noreferrer"}
+                style={{ textDecoration: "underline" }}
+              >
+                EIF 3.0
+              </a>{" "}
+              with a goal to create better habits for writing more secure
+              contracts.
+            </Text>
+          </Flex>
+          <Flex gap={"20.35px"}>
+            <Button
+              height={"47px"}
+              paddingRight={"25px"}
+              paddingLeft={"25px"}
+              rounded={"4px"}
+              bg={"#528D51"}
+              fontSize={"20px"}
+              fontWeight={"normal"}
+              _hover={{ background: "" }}
+              _active={{}}
+            >
+              Start Solving
+            </Button>
+            <Button
+              height={"47px"}
+              paddingRight={"25px"}
+              paddingLeft={"25px"}
+              rounded={"4px"}
+              bg={"rgba(255, 255, 255, 0.04)"}
+              border={"1px solid rgba(255, 255, 255, 0.1)"}
+              fontSize={"20px"}
+              fontWeight={"normal"}
+              _hover={{}}
+              _active={{}}
+            >
+              Learn First
+            </Button>
+          </Flex>
+        </Flex>
+
+        <Flex justify={"center"} userSelect={"none"}>
+          <Image src={"hero.svg"} alt={"hero"} />
         </Flex>
       </Flex>
     </Flex>
